252642974b45052f1f7ead982a9134490b0ffe3a
Natan__Brito <natanismaelbrito@gmail.com>
2022-05-07 18:16:46 -0300

    clean done

--- src/components/TelaLogin.js
+++ src/components/TelaLogin.js
@@ -1,8 +1,16 @@
 import styledComponents from "styled-components";
+// import Logo from "./../assets/images/MyWallet.png"; dando problema nesse import
 export default function TelaLogin(){
     return(
         <div className="App">
+        <LogoImage>
+        {/* <img src={Logo} alt="logo"/> */}
+        </LogoImage>
         <h1 className="xx"> "cheguei mermão, Tela Login"</h1>
         </div>
     )
 }
+const LogoImage= styledComponents.div`
+
+
+`
